75cb64b3d9b58a45899dd14413a5c1c2583fd74d
Stepan Bolshakov <stepan.bolshakov@gmail.com>
2015-12-14 10:01:23 +0300

    Questions model, controller and specs

--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -1,6 +1,12 @@
 FactoryGirl.define do
   factory :question do
-    
+    title "My Question"
+    body "Question Body"
+  end
+
+  factory :invalid_question, class: 'Question' do
+    title nil
+    body nil
   end
 
 end
